b65e15d5b3ec8589f4905a2b429c14a01e4ec5e4
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-06-22 19:05:48 +0300

    Create a game class and a game object

--- main.rb
+++ main.rb
@@ -1,31 +1,37 @@
-colors = %w[red blue yellow green black white]
+# frozen_string_literal: true
 
-codemaker = []
-colors.shuffle.each do |item|
-  break if codemaker.length == 4
-  codemaker.push(item)
-end
+class Game
+  colors = %[red blue yellow green black white]
 
-puts "Enter your pattern"
-codebreaker = gets.chomp.split(',')
+  codemaker = []
+  colors.shuffle.each do |item|
+    break if codemaker.length == 4
 
-def existence(codebreaker, codemaker)
-  i = 0
-  while i < 4
-    codemaker.include?(codebreaker[i])
-    p "#{codebreaker[i]} exists" if codemaker.include?(codebreaker[i]) == true
-    i += 1
+    codemaker.push(item)
   end
-  position(codebreaker,codemaker)
-end
 
-def position(codebreaker, codemaker)
-  i = 0
-  while i < 4
-    codebreaker[i].eql?(codemaker[i])
-    p "#{codeguess[i]} is in the right position" if codebreaker[i].eql?(codemaker[i]) == true
-    i += 1
+  puts 'Enter your pattern'
+  gets.chomp.split(',')
+
+  def existence(codebreaker, codemaker)
+    i = 0
+    while i < 4
+      codemaker.include?(codebreaker[i])
+      p "#{codebreaker[i]} exists" if codemaker.include?(codebreaker[i]) == true
+      i += 1
+    end
+    position(codebreaker, codemaker)
+  end
+
+  def position(codebreaker, codemaker)
+    i = 0
+    while i < 4
+      codebreaker[i].eql?(codemaker[i])
+      p "#{codebreaker[i]} is in the right position" if codebreaker[i].eql?(codemaker[i]) == true
+      i += 1
+    end
   end
 end
 
-existence(codebreaker,codemaker)
+game = Game.new
+existence(codebreaker, codemaker)
