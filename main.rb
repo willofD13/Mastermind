e77a98f4e53de16e848172926a9697460b3ca99f
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-06-22 18:54:35 +0300

    Check the existence codebreaker colors

--- main.rb
+++ main.rb
@@ -1,9 +1,19 @@
-colors = ['red','blue','yellow','green','black','white']
+colors = %w[red blue yellow green black white]
 
 codemaker = []
 colors.shuffle.each do |item|
   break if codemaker.length == 4
+
   codemaker.push(item)
 end
 
-codebreaker = gets.chomp.split(',')
+gets.chomp.split(',')
+
+def existence(codebreaker, codemaker)
+  i = 0
+  while i < 4
+    codemaker.include?(codebreaker[i])
+    p "#{codebreaker[i]} exists" if codemaker.include?(codebreaker[i]) == true
+    i += 1
+  end
+end
