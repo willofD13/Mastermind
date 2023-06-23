288cdbdb04b34af2cef258c3a3fa4f81552a33cf
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-06-23 12:16:08 +0300

    Improve the feedback on codebreaker

--- main.rb
+++ main.rb
@@ -1,69 +1,78 @@
 # frozen_string_literal: true
-##require 'pry-byebug'
+
+# #require 'pry-byebug'
 class Game
-  
-  @@colors = ['red','blue','yellow','green','black','white']
+  @@colors = %w[red blue yellow green black white]
   @@turn = 1
 
-  attr_reader :codemaker,:codebreaker
+  attr_reader :codemaker, :codebreaker
 
   def initialize
     @codebreaker = []
     @codemaker = []
   end
-  
+
   def ask_user
-    puts "Do you want to be the codemaker or the codebreaker?"
+    puts 'Do you want to be the codemaker or the codebreaker?'
     choice = gets.chomp
-      if choice == 'codebreaker'
-        maker_pattern
-        while @@turn <=12
-          breaker_pattern(codebreaker)
-          @@turn += 1
-        end
-      elsif choice == 'codemaker'
-      end
+    return unless choice == 'codebreaker'
+
+    maker_pattern
+    while @@turn <= 12
+      breaker_pattern(codebreaker)
+      @@turn += 1
+    end
   end
 
   def maker_pattern
     @@colors.shuffle.each do |item|
       break if codemaker.length == 4
+
       codemaker.push(item)
     end
     p codemaker
   end
-  
-  def breaker_pattern (codebreaker)
+
+  def breaker_pattern(codebreaker)
     puts 'Enter your pattern. Please separate your color choices with comma(,)'
     codebreaker = gets.chomp.split(',')
-    existence(codebreaker,codemaker)
+    existence(codebreaker, codemaker)
   end
-    
 
   def existence(codebreaker, codemaker)
-    num = 0
+    x = 0
     i = 0
     while i < 4
       codemaker.include?(codebreaker[i])
-      num += 1 if codemaker.include?(codebreaker[i]) == true
+      x += 1 if codemaker.include?(codebreaker[i]) == true
       i += 1
     end
-    p "#{num} colors exist in code pattern"
+    if x <= 1
+      puts "#{x} color exists in code pattern"
+    elsif x > 1
+      puts "#{x} colors exist in code pattern"
+    end
     position(codebreaker, codemaker)
   end
 
   def position(codebreaker, codemaker)
+    y = 0
     i = 0
     while i < 4
       codebreaker[i].eql?(codemaker[i])
-        if codebreaker == codemaker
-          puts "You found the pattern!"
-          @@turn = 13
-          break;
-        elsif codebreaker[i].eql?(codemaker[i]) == true
-          puts "#{codebreaker[i]} is in the right position"
-        end
+      if codebreaker == codemaker
+        puts 'You found the pattern!'
+        @@turn = 13
+        break
+      elsif codebreaker[i].eql?(codemaker[i]) == true
+        y += 1
+      end
       i += 1
+    end
+    if y <= 1
+      puts "#{y} is in the right position"
+    elsif y > 1
+      puts "#{y} are in the right position"
     end
   end
 
