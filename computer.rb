a43fbc70ae2f8223cf94bff75f34ed8429f50440
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-06-26 20:32:02 +0300

    Improve the white peg counting

--- computer.rb
+++ computer.rb
@@ -1,3 +1,4 @@
+require 'pry-byebug'
 class Game
   @@pool = ['red','blue','black','white','yellow','green'].repeated_permutation(4).to_a
   
@@ -6,12 +7,13 @@ class Game
   def initialize
     @guess = []
     @answer = []
-    @all_scores = Hash.new {|g,a| g[a] = {}}
+    @all_scores = Hash.new {|h,k| h[k] = {}}
   end
 
   def play
     answer = @@pool.sample
-    calculate_score(['red','red','blue','blue'],answer)
+    first_score = calculate_score(['red','red','blue','blue'],answer)
+    store(first_score,answer)
   end
 
   def calculate_score (guess,answer)
@@ -35,21 +37,26 @@ class Game
       end
       i += 1
     end
-
-    i = 0
-    while i < 4
-      b.include?(a[i])
-      white_peg += 1 if b.include?(a[i]) == true
-      i += 1
+    
+    c = a.zip(b)
+    c.each do |i|
+      i.each do |element|
+        if element[0] == element[1]
+          white_peg += 1
+        end
+      end
     end
     score = "#{white_peg},#{black_peg}"   
-    puts score
+    return score
   end
 
-  def store
-    @@pool.each do |i|
-      @all_scores = calculate_score(i,answer)
+  def store (first_score,answer)
+    binding.pry
+    @@pool.each do |item|
+      @all_scores[item] = calculate_score(item,answer)
     end
+    new_pool = @@pool.keep_if { |element| @all_scores[element] == first_score}
+    p new_pool
   end
 
 end
