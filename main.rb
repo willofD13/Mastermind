165cef6073741fea9643eb310f63bd892a55ee2e
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-06-26 17:30:22 +0300

    Change the way score is calculated

--- main.rb
+++ main.rb
@@ -44,27 +44,19 @@ class Game
   def breaker_pattern(codebreaker)
     puts 'Enter your pattern. Please separate your color choices with comma(,)'
     codebreaker = gets.chomp.split(',')
-    existence(codebreaker, codemaker)
+    calculate_score(codebreaker, codemaker)
   end
 
-  def existence(codebreaker, codemaker)
-    x = 0
+  def calculate_score(codebreaker, codemaker)
+    white_peg = 0
+    black_peg = 0
     i = 0
     while i < 4
       codemaker.include?(codebreaker[i])
-      x += 1 if codemaker.include?(codebreaker[i]) == true
+      white_peg += 1 if codemaker.include?(codebreaker[i]) == true
       i += 1
     end
-    if x <= 1
-      puts "#{x} color exists in code pattern"
-    elsif x > 1
-      puts "#{x} colors exist in code pattern"
-    end
-    position(codebreaker, codemaker)
-  end
 
-  def position(codebreaker, codemaker)
-    y = 0
     i = 0
     while i < 4
       codebreaker[i].eql?(codemaker[i])
@@ -73,15 +65,11 @@ class Game
         @@turn = 13
         break
       elsif codebreaker[i].eql?(codemaker[i]) == true
-        y += 1
+        black_peg += 1
       end
       i += 1
     end
-    if y <= 1 
-      puts "#{y} is in the right position"
-    elsif y > 1 
-      puts "#{y} are in the right position"
-    end
+    puts "#{white_peg},#{black_peg}"
   end
 
 end
