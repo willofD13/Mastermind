7cd84a4c7aa9d633abea9478d9ea24d365f74be0
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-06-27 15:43:57 +0300

    Initialize first variables

--- computer.rb
+++ computer.rb
@@ -1,4 +1,8 @@
 class Game
+  def initialize
+    @all_answers = ['red','blue','black','green','white','yellow'].repeated_permutations(4).to_a
+    @guess = []
+    @answer = []
 end 
 
 game = Game.new
