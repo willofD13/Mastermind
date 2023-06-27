0d3f23a48f292ee46e2f3fa8a02b01bdd2513342
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-06-27 20:03:17 +0300

    Reduce answers for next guesses

--- computer.rb
+++ computer.rb
@@ -1,28 +1,21 @@
-class Game
+#equire 'pry-byebug'
+
+class Computer
   @@turn = 1
 
-  attr_accessor :guess,:answer,:all_answers
+  attr_accessor :guess,:answer,:all_answers,:all_scores
 
-  def initialize
-    @all_answers = ['red','blue','black','green','white','yellow'].repeated_permutations(4).to_a
-    @guess = []
-    @answer = []
-    @all_scores = Hash.new {|h,k| h[k] = {}}
-
-    @all_answers.product(@all_answers).each do |guess,answer|
-      @all_scores[guess,answer] = calculate_score(guess,answer)
-  end 
-  
   def play 
     answer = all_answers.sample
     @possible_answers = all_answers.dup 
     @possible_scores = all_scores.dup
     while @@turn <=12
-      @guess = make_guess
+      guess = make_guess
       if all_answers.include?(guess)
         @@turn += 1
         @score = calculate_score(guess,answer)
-        if @score = '0,4'
+        if @score == '0,4'
+          @@turn = 13
           puts 'Pattern found'
           break
         end
@@ -30,11 +23,6 @@ class Game
     end 
   end
 
-  def make_guess
-    if @@turn == 1
-      guess = ['red','red','blue','blue']
- 
-  
 
   def calculate_score (guess,answer)
     a = []
@@ -46,11 +34,7 @@ class Game
     i = 0
     while i < 4
       guess[i].eql?(answer[i])
-      if guess == answer
-        puts 'You found the pattern!'
-        @@turn = 13
-        break
-      elsif guess[i].eql?(answer[i]) == true
+      if guess[i].eql?(answer[i]) == true
         black_peg += 1
       else a.push(guess[i])
            b.push(answer[i])
@@ -58,15 +42,42 @@ class Game
       i += 1
     end
 
-    c = a.zip(b)
-    c.each do |i|
-      if i[0] == i[1]
+    a.each do |peg|
+      if b.include?(peg)
+        b.delete(peg)
         white_peg += 1
       end
     end
     score = "#{white_peg},#{black_peg}"   
-    puts score
+    score
   end
+
+  def initialize
+    @all_answers = ['red','blue','black','green','white','yellow'].repeated_permutation(4).to_a
+    @guess = []
+    @answer = []
+    @all_scores = Hash.new {|h,k| h[k] = {}}
+   
+    @all_answers.product(@all_answers).each do |guess,answer|
+      @all_scores[guess][answer] = calculate_score(guess,answer)
+    end
+  end 
+  
+  
+  def make_guess
+    if @@turn == 1
+      guess = ['red','red','blue','blue']
+    elsif @@turn > 1
+      pool = @possible_answers.keep_if {|answer|
+        @possible_scores[guess][answer] == @score
+      }
+      puts pool.size
+      guess = pool.sample
+
+    end
+  end
+
 end
 
-game = Game.new
+game = Computer.new 
+game.play
