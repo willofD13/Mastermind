500a42443306edb749207465e34b157d172d1041
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-06-27 16:10:49 +0300

    Complete play method

--- computer.rb
+++ computer.rb
@@ -14,32 +14,29 @@ class Game
     answer = all_answers.sample
     @possible_answers = all_answers.dup 
     @possible_scores = all_scores.dup
+    while @@turn <=12
+      @guess = make_guess
+      if all_answers.include?(guess)
+        @@turn += 1
+        @score = calculate_score(guess,answer)
+        if @score = '0,4'
+          puts 'Pattern found'
+          break
+        end
+      end
+    end 
   end
 
   first_guess = ['red','red','blue','blue']
+ 
   
-  def create_answer (array)
-     array.sample 
-  end 
-  
-  while @@turn <=12
-    @guess = make_guess
-    if all_answers.include?(guess)
-      @@turn += 1
-      @score = calculate_score(guess,answer)
-      if @score = '0,4'
-        puts 'Pattern found'
-        break
-      end
-    end
-  end 
-  
+
   def calculate_score (guess,answer)
     a = []
     b = []
     white_peg = 0
     black_peg = 0
-    score = 0
+    score = ''
     
     i = 0
     while i < 4
@@ -64,5 +61,7 @@ class Game
     end
     score = "#{white_peg},#{black_peg}"   
     puts score
+  end
+end
 
 game = Game.new
