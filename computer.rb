7291db373a59e202241f2ef68acacdabe8c56275
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-06-27 15:45:15 +0300

    Create getter and setter methods

--- computer.rb
+++ computer.rb
@@ -1,4 +1,7 @@
 class Game
+
+  attr_accessor :guess,:answer,:all_answers
+
   def initialize
     @all_answers = ['red','blue','black','green','white','yellow'].repeated_permutations(4).to_a
     @guess = []
