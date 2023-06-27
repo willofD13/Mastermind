52898eeaaec2ed05f1602b19b09c5af0f3be0157
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-06-27 15:51:32 +0300

    Create answer and give feedback score for each guess

--- computer.rb
+++ computer.rb
@@ -6,6 +6,43 @@ class Game
     @all_answers = ['red','blue','black','green','white','yellow'].repeated_permutations(4).to_a
     @guess = []
     @answer = []
-end 
+  end 
+
+  first_guess = ['red','red','blue','blue']
+  
+  def create_answer (array)
+     array.sample 
+  end 
+
+  def calculate_score (guess,answer)
+    a = []
+    b = []
+    white_peg = 0
+    black_peg = 0
+    score = 0
+    
+    i = 0
+    while i < 4
+      guess[i].eql?(answer[i])
+      if guess == answer
+        puts 'You found the pattern!'
+        @@turn = 13
+        break
+      elsif guess[i].eql?(answer[i]) == true
+        black_peg += 1
+      else a.push(guess[i])
+           b.push(answer[i])
+      end
+      i += 1
+    end
+
+    c = a.zip(b)
+    c.each do |i|
+      if i[0] == i[1]
+        white_peg += 1
+      end
+    end
+    score = "#{white_peg},#{black_peg}"   
+    puts score
 
 game = Game.new
